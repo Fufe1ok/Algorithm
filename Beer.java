36f70c7a19f7064d94292444ad9648e749d75019
Fufe1ok <32967290+Fufe1ok@users.noreply.github.com>
2018-12-04 17:21:12 +0200

    Add files via upload

---
+++ Beer.java
@@ -0,0 +1,24 @@
+public class Beer {
+    private static int choose(String[] beers) {
+        int n = beers.length, m = beers[0].length();
+        int[] beer = new int[n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                beer[i] = beer[i] << 1;
+                beer[i] += (beers[i].charAt(j) == 'Y') ? 1 : 0;
+            }
+        int min = Integer.MAX_VALUE;
+        loop: for (int i = 1; i < (1 << m); i++) {
+            for (int j = 0; j < n; j++)
+                if ((i & beer[j]) == 0)
+                    continue loop;
+            min = Math.min(Integer.bitCount(i), min);
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        String[] beers = new String[]{"YNY", "NYY", "NYY", "YNY", "YNY"};
+        System.out.println(choose(beers));
+    }
+}
